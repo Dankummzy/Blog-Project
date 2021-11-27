e2d5fbef2f37f63b968ccbbca311906b8b101478
Daniel Kumaga <danterkum16@gmail.com>
2021-11-27 03:46:12 -0800

    Added the detailed view functionality

--- codehub/urls.py
+++ codehub/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArticlesListView
+from .views import ArticlesListView, ArticlesDetailView
 
 
 urlpatterns = [
-    path('', ArticlesListView.as_view(), name='Home')
+    path('', ArticlesListView.as_view(), name='Home'),
+    path('article/<int:pk>/', ArticlesDetailView.as_view(), name='Detail'),
 ]
--- codehub/views.py
+++ codehub/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Articles
 
 
@@ -9,3 +9,6 @@ class ArticlesListView(ListView):
     context_object_name = 'Articles'
     ordering = ['-date']
 
+
+class ArticlesDetailView(DetailView):
+    model = Articles
